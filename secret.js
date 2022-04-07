24d1418ade5e207bd48024d88ab1c21706f0ad25
Blake Mason <826688@stma.k12.mn.us>
2022-04-07 11:57:02 -0500

    4/7/22

--- secret.js
+++ secret.js
@@ -1,1 +1,16 @@
-document.getElementById("password").value 
+
+function secret() {
+	
+
+if (document.getElementById("password").value == "12345") {
+	document.getElementById("pass").innerHTML = "Access Granted"
+	}
+	
+
+if (document.getElementById("password").value != "12345") {
+	document.getElementById("pass").innerHTML = "Access Denied"
+	}
+}
+
+
+document.getElementById("press").addEventListener("click", secret)
